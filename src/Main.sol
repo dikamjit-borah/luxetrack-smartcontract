c392646277a3db93b75508b5b452197438b1cc5b
dikamjit-borah <dikamjitborah.devacc01@gmail.com>
2025-02-23 13:32:50 +0530

    users can add only to their notebooks

--- src/Main.sol
+++ src/Main.sol
@@ -6,14 +6,21 @@ contract Notes {
         string title;
         string description;
     }
+    // Each address has their own notebook
+    mapping(address => Note[]) private notebooks;
 
-    Note[] public notebook;
+    // log notes
+    event NoteAdded(address indexed user, string title, string description);
 
-    function addToNotebook(string memory title, string memory description) public {
-        notebook.push(Note(title, description)); 
+    function addToNotebook(
+        string memory title,
+        string memory description
+    ) public {
+        notebooks[msg.sender].push(Note(title, description));
+        emit NoteAdded(msg.sender, title, description);
     }
 
-    function getNotes() public view returns (Note[] memory) {
-        return notebook;
+    function getMyNotes() public view returns (Note[] memory) {
+        return notebooks[msg.sender]; // Only returns the sender’s notes
     }
 }
